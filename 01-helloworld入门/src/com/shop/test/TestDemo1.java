431cc1899f25b993e932cc2b08362053fbf92511
ximenqing <372837@qq.com>
2019-11-08 16:22:07 +0800

    xx

--- 01-helloworld入门/src/com/shop/test/TestDemo1.java
+++ 01-helloworld入门/src/com/shop/test/TestDemo1.java
@@ -5,23 +5,5 @@ public interface TestDemo1 {
 	// 张三第一次添加代码
 	void addUser();
 	
-	// 张三 添加了一个helloworld 方法
-	void  hellworld();
-	
-	
-	// 张三添加了aa 
-	void aa();
-	
-	
-	// 李四下贱
-void lisibb();
-
-
-// 李四
-void lisiccc();
-
-
-// 李四  xxx
-void   lisi_xxxx();
 
 }
